07de891f136a4cfba9af28551ef5ed56360c2c72
manumay@1962 <manumay8@gmail.com>
2025-08-25 03:13:34 +0530

    first commit

---
+++ client/src/components/Sidebar.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import assets from '../assets/assets'
+
+function Sidebar({selectedUser,setSelectedUser}) {
+  return (
+    <div>
+        <div className='pb-5'>
+            <div className='flex justify-between items-center '>
+                <img src={assets.logo} alt="logo" className='max-w-40' />
+                <div className='relative py-2 group'>
+                <img src={assets.menu_icon} alt="Menu" className='max-h-5 cursor-pointer' />
+                </div>
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default Sidebar
