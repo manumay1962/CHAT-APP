6ee9162883491ccbc9ecc1a2d3ca652a38374c80
manumay@1962 <manumay8@gmail.com>
2025-08-26 20:57:49 +0530

    frontend made

--- client/src/pages/HomePage.jsx
+++ client/src/pages/HomePage.jsx
@@ -1,21 +1,35 @@
-import React, { useState } from 'react'
-import Sidebar from '../components/Sidebar'
-import ChatContainer from '../components/ChatContainer'
-import RightSideBar from '../components/RightSideBar'
+import React, { useState } from "react";
+import Sidebar from "../components/Sidebar";
+import ChatContainer from "../components/ChatContainer";
+import RightSideBar from "../components/RightSideBar";
 
 function HomePage() {
-
-  const [selectedUser,setSelectedUser] = useState(false);
+  const [selectedUser, setSelectedUser] = useState(false);
   return (
-    <div className='border w-full h-screen sm:px-[15%]  sm:py-[5%]'>
-      <div className={`backdrop-blur-xl border-2 border-gray-600 rounded-2xl overflow-hidden h-[100%]
-      grid grid-cols-1 relative  ${selectedUser ? 'md:grid-cols-[1fr_1.5fr_1fr] xl:grid-cols-[1fr_2fr_1fr]' : 'md:grid-cols-2'}`}>
-        <Sidebar/>
-        <ChatContainer />
-        <RightSideBar />
+    <div className="border w-full h-screen sm:px-[15%]  sm:py-[5%]">
+      <div
+        className={`backdrop-blur-xl border-2 border-gray-600 rounded-2xl overflow-hidden h-[100%]
+      grid grid-cols-1 relative  ${
+        selectedUser
+          ? "md:grid-cols-[1fr_1.5fr_1fr] xl:grid-cols-[1fr_2fr_1fr]"
+          : "md:grid-cols-2"
+      }`}
+      >
+        <Sidebar
+          selectedUser={selectedUser}
+          setSelectedUser={setSelectedUser}
+        />
+        <ChatContainer
+          selectedUser={selectedUser}
+          setSelectedUser={setSelectedUser}
+        />
+        <RightSideBar
+          selectedUser={selectedUser}
+          setSelectedUser={setSelectedUser}
+        />
       </div>
     </div>
-  )
+  );
 }
 
-export default HomePage
+export default HomePage;
